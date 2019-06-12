80ff5e78057091c7a44ed7aaadd9fb0e16793b46
Ily <ilyrodriguez@gmail.com>
2019-06-11 21:28:59 -0400

    Update htmlRoutes.js

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,39 +1,44 @@
 var db = require("../models")
-var express = require("express");
-var router = express.Router();
 
-// Load index page
-	router.get("/", function (req, res) {
+
+module.exports = function (app) {
+	// Load index page
+	app.get("/", function (req, res) {
 		res.render("index")
 	})
 
-	//this is the dashboard route. Using mock data to pretend data is being sent from the backend to test handlebars
-	router.get("/dashboard", function(req, res) {
-		res.render("dashboard", {
-			username: "Im_a_fake_username",
-			challengeOfTheDay: "This_a_mock_challenge" 
-		})
+	app.get("/dashboard", function(req, res){
+		const goals=[{
+			title: "New Car",
+			id: 1
+		},{
+			title: "Get a dev job",
+			id: 2
+		}]
+		const data= {
+			username: "whatever",
+			challengeOfTheDay: "This_a_mock_challenge",
+			userGoals: goals 
+		}
+		res.render("dashboard", data)
 	})
 
 
-
-
 	// Load example page and pass in an example by id
-	router.get("/example/:id", function (req, res) {
-		db.Example.findOne({
+	app.get("/example/:id", function (req, res) {
+		db.goal.findAll({
 			where: {
-				id: req.params.id
+				userId: req.params.id
 			}
-		}).then(function (dbExample) {
-			res.render("example", {
-				example: dbExample
+		}).then(function (userGoals) {
+			res.render("dashboard", {
+				userGoals: userGoals
 			})
 		})
 	})
 
-	// // Render 404 page for any unmatched routes
-	// router.get("*", function (req, res) {
-	// 	res.render("404")
-	// })
-
-module.exports=router;
+	// Render 404 page for any unmatched routes
+	app.get("*", function (req, res) {
+		res.render("404")
+	})
+}
