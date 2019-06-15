eeec83b37f66de478c07db1c6bb51b595aadc176
Priscila Heller <priscila.x.p.r@gmail.com>
2019-06-15 10:04:39 -0400

    paypal integration done

--- server.js
+++ server.js
@@ -35,10 +35,12 @@ app.set("view engine", "handlebars");
 //routes
 var htmlroutes = require("./routes/htmlRoutes");
 var apiroutes = require("./routes/apiRoutes");
+// var paypalroute = require("./controllers/paypal")
 app.use(htmlroutes);
 app.use(apiroutes);
+// app.use(paypalroute);
 
-//PayPal
+//======== PayPal starts here ========//
 paypal.configure({
   'mode': 'sandbox', //sandbox or live
   client_id:process.env.PAYPAL_ID,
@@ -47,43 +49,43 @@ paypal.configure({
 
 app.post('/pay', (req, res) => {
   var create_payment_json = {
-    "intent": "sale",
-    "payer": {
-        "payment_method": "paypal"
-    },
-    "redirect_urls": {
-        "return_url": "http://localhost:3000/dashboard",
-        "cancel_url": "http://localhost:3000/cancel"
-    },
-    "transactions": [{
-        "item_list": {
-            "items": [{
-                "name": "set.com Beta subscription",
-                "sku": "001",
-                "price": "25.00",
-                "currency": "USD",
-                "quantity": 1
-            }]
-        },
-        "amount": {
-            "currency": "USD",
-            "total": "25.00"
-        },
-        "description": "set.com Beta subscription"
-    }]
-};
+      "intent": "sale",
+      "payer": {
+          "payment_method": "paypal"
+      },
+      "redirect_urls": {
+          "return_url": "http://localhost:3000/dashboard",
+          "cancel_url": "http://localhost:3000/cancel"
+      },
+      "transactions": [{
+          "item_list": {
+              "items": [{
+                  "name": "set.com Beta subscription",
+                  "sku": "001",
+                  "price": "25.00",
+                  "currency": "USD",
+                  "quantity": 1
+              }]
+          },
+          "amount": {
+              "currency": "USD",
+              "total": "25.00"
+          },
+          "description": "set.com Beta subscription"
+      }]
+  };
 
-paypal.payment.create(create_payment_json, function (error, payment) {
-  if (error) {
-      throw error;
-  } else {
-      for (var i = 0; i < payment.links.length; i++) {
-        if(payment.links[i].rel === "approval_url") {
-          res.redirect(payment.links[i].href);
-        }
+  paypal.payment.create(create_payment_json, function (error, payment) {
+      if (error) {
+          throw error;
+      } else {
+          for (var i = 0; i < payment.links.length; i++) {
+              if (payment.links[i].rel === "approval_url") {
+                  res.redirect(payment.links[i].href);
+              }
+          }
       }
-  }
-});
+  });
 
 });
 
@@ -92,29 +94,28 @@ app.get('/dashboard', (req, res) => {
   var paymentId = req.query.paymentId;
 
   var execute_payment_json = {
-    "payer_id": payerId,
-    "transactions": [{
-        "amount": {
-            "currency": "USD",
-            "total": "25.00"
-        }
-    }]
+      "payer_id": payerId,
+      "transactions": [{
+          "amount": {
+              "currency": "USD",
+              "total": "25.00"
+          }
+      }]
   };
 
   paypal.payment.execute(paymentId, execute_payment_json, function (error, payment) {
-    if (error) {
-        console.log(error.response);
-        throw error;
-    } else {
-        console.log("Get Payment Response");
-        console.log(JSON.stringify(payment));
-    }
-});
+      if (error) {
+          console.log(error.response);
+          throw error;
+      } else {
+          console.log("Get Payment Response");
+          console.log(JSON.stringify(payment));
+      }
+  });
 });
 
 app.get('/cancel', (req, res) => res.send("Cancelled"));
-
-
+// ============ PayPal ends here ======================= //
 
 
 //start the server and sync the database
