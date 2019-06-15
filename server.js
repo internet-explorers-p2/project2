f44353eb65ff8817b397cef5267c525d2ca7df71
Priscila Heller <priscila.x.p.r@gmail.com>
2019-06-15 10:54:31 -0400

    paypal works my friends :tada:

--- server.js
+++ server.js
@@ -35,10 +35,10 @@ app.set("view engine", "handlebars");
 //routes
 var htmlroutes = require("./routes/htmlRoutes");
 var apiroutes = require("./routes/apiRoutes");
-// var paypalroute = require("./controllers/paypal")
+var paypalroute = require("./controllers/paypal")
 app.use(htmlroutes);
 app.use(apiroutes);
-// app.use(paypalroute);
+app.use(paypalroute);
 
 //======== PayPal starts here ========//
 paypal.configure({
@@ -47,74 +47,7 @@ paypal.configure({
   client_secret:process.env.PAYPAL_SECRET
 });
 
-app.post('/pay', (req, res) => {
-  var create_payment_json = {
-      "intent": "sale",
-      "payer": {
-          "payment_method": "paypal"
-      },
-      "redirect_urls": {
-          "return_url": "http://localhost:3000/dashboard",
-          "cancel_url": "http://localhost:3000/cancel"
-      },
-      "transactions": [{
-          "item_list": {
-              "items": [{
-                  "name": "set.com Beta subscription",
-                  "sku": "001",
-                  "price": "25.00",
-                  "currency": "USD",
-                  "quantity": 1
-              }]
-          },
-          "amount": {
-              "currency": "USD",
-              "total": "25.00"
-          },
-          "description": "set.com Beta subscription"
-      }]
-  };
 
-  paypal.payment.create(create_payment_json, function (error, payment) {
-      if (error) {
-          throw error;
-      } else {
-          for (var i = 0; i < payment.links.length; i++) {
-              if (payment.links[i].rel === "approval_url") {
-                  res.redirect(payment.links[i].href);
-              }
-          }
-      }
-  });
-
-});
-
-app.get('/dashboard', (req, res) => {
-  var payerId = req.query.PayerID;
-  var paymentId = req.query.paymentId;
-
-  var execute_payment_json = {
-      "payer_id": payerId,
-      "transactions": [{
-          "amount": {
-              "currency": "USD",
-              "total": "25.00"
-          }
-      }]
-  };
-
-  paypal.payment.execute(paymentId, execute_payment_json, function (error, payment) {
-      if (error) {
-          console.log(error.response);
-          throw error;
-      } else {
-          console.log("Get Payment Response");
-          console.log(JSON.stringify(payment));
-      }
-  });
-});
-
-app.get('/cancel', (req, res) => res.send("Cancelled"));
 // ============ PayPal ends here ======================= //
 
 
