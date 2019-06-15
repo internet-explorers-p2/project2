2905555d05d5c556fa9418bfaff30f10ebb7502a
Priscila Heller <priscila.x.p.r@gmail.com>
2019-06-15 02:33:32 -0400

    Paypal works, need to confirm money is being sent

--- controllers/google-auth.js
+++ controllers/google-auth.js
@@ -18,7 +18,7 @@ passport.deserializeUser((id,done) => {
 //   credentials (in this case, an accessToken, refreshToken, and Google
 //   profile), and invoke a callback with a user object.
 passport.use(new GoogleStrategy({
-    clientID:process.env.CLIENT_ID ,
+    clientID:process.env.CLIENT_ID,
     clientSecret:process.env.CLIENT_SECRET,
     callbackURL: "/google/redirect"
   },
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var PORT = process.env.PORT || 3000;
 var db = require("./models");
 var app = express();
 var cookieSession = require("cookie-session");
-// var Handlebars = require("handlebars");
+var paypal = require('paypal-rest-sdk');
 
 //cookie
 app.use(cookieSession({
@@ -20,7 +20,6 @@ app.use(cookieSession({
   saveUninitialized: false
 }));
 
-// app.use(express.static(__dirname + "public"));
 app.use(express.static("."));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -33,15 +32,92 @@ app.use(passport.session())
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+//routes
 var htmlroutes = require("./routes/htmlRoutes");
 var apiroutes = require("./routes/apiRoutes");
-// var dashboardroutes = require("./routes/dashboardRoutes");
-
 app.use(htmlroutes);
 app.use(apiroutes);
-// app.use(dashboardroutes);
+
+//PayPal
+paypal.configure({
+  'mode': 'sandbox', //sandbox or live
+  client_id:process.env.PAYPAL_ID,
+  client_secret:process.env.PAYPAL_SECRET
+});
+
+app.post('/pay', (req, res) => {
+  var create_payment_json = {
+    "intent": "sale",
+    "payer": {
+        "payment_method": "paypal"
+    },
+    "redirect_urls": {
+        "return_url": "http://localhost:3000/dashboard",
+        "cancel_url": "http://localhost:3000/cancel"
+    },
+    "transactions": [{
+        "item_list": {
+            "items": [{
+                "name": "set.com Beta subscription",
+                "sku": "001",
+                "price": "25.00",
+                "currency": "USD",
+                "quantity": 1
+            }]
+        },
+        "amount": {
+            "currency": "USD",
+            "total": "25.00"
+        },
+        "description": "set.com Beta subscription"
+    }]
+};
+
+paypal.payment.create(create_payment_json, function (error, payment) {
+  if (error) {
+      throw error;
+  } else {
+      for (var i = 0; i < payment.links.length; i++) {
+        if(payment.links[i].rel === "approval_url") {
+          res.redirect(payment.links[i].href);
+        }
+      }
+  }
+});
+
+});
+
+app.get('/dashboard', (req, res) => {
+  var payerId = req.query.PayerID;
+  var paymentId = req.query.paymentId;
+
+  var execute_payment_json = {
+    "payer_id": payerId,
+    "transactions": [{
+        "amount": {
+            "currency": "USD",
+            "total": "25.00"
+        }
+    }]
+  };
+
+  paypal.payment.execute(paymentId, execute_payment_json, function (error, payment) {
+    if (error) {
+        console.log(error.response);
+        throw error;
+    } else {
+        console.log("Get Payment Response");
+        console.log(JSON.stringify(payment));
+    }
+});
+});
+
+app.get('/cancel', (req, res) => res.send("Cancelled"));
 
 
+
+
+//start the server and sync the database
 db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
     console.log("Server listening on: http://localhost:" + PORT);
